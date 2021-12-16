4e5e8dda1a284bdb003239409426b6067e13ccfa
unknown <hazratzn@code.edu.az>
2021-12-16 03:57:02 -0800

    Delegates

---
+++ Delegates/Delegates/MethodDelegates.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates1
+{
+    public class MethodDelegates
+    {
+        public delegate bool Check(int num);
+        public delegate void MathOperators(int a, int b);
+        public delegate void ChangeNames(string str);
+        public static void Sum(int a, int b)
+        {
+            Console.WriteLine((a + b));
+        }
+        public static void Multiply(int a, int b)
+        {
+            Console.WriteLine((a * b));
+        }
+
+
+        public static void StringToUpper(string str, int n)
+        {
+            Console.WriteLine(str.ToUpper()+ " " +n);
+        }
+        public static void StringToLower(string str, int n)
+        {
+            Console.WriteLine(str.ToLower()+ " " + n);
+        }
+        public static bool CheckEven(int num)
+        {
+            return num % 2 == 0;
+        }
+
+        public static bool CheckNum(int num)
+        {
+            return num > 5;
+        }
+
+        public static int Sum(Predicate<int> func, params int[] nums)
+        {
+            var sum = 0;
+            foreach (var item in nums)
+            {
+                if (func(item))
+                {
+                    sum += item;
+                }
+            }
+
+            return sum;
+
+        }
+    }
+}
---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Delegates1;
+using static Delegates1.MethodDelegates;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // MathOperators test = Sum;
+            //test += Multiply;
+
+            //test(9, 8);
+
+            //ChangeNames names = StringToUpper;
+            ////names += StringToLower;
+            //names += x => Console.WriteLine(x.ToLower());
+            //names.Invoke ("hazrat");
+
+            //names("hazrat");
+
+            //Console.WriteLine(Sum(m => m%2 == 0, 1, 2, 3, 4, 5, 6, 7));
+            //Console.WriteLine(Sum(CheckEven, 1, 2, 3, 4, 5, 6, 7));
+            //Console.WriteLine(Sum(CheckNum, 1, 2, 3, 4, 5, 6, 7));
+
+            //Action<string,int> names = StringToUpper;
+            //names += StringToLower;
+            //names("BEXtiyar",5);
+            //names("Rovshen", 10);
+
+
+        }
+    }
+}
